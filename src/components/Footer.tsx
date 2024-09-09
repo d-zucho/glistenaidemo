5aa862323f0d1d9b6be889caac113ca3890b344a
D_Zucho <danny.nico.dev@outlook.com>
2024-09-09 02:56:26 -0700

    initial setup

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import WordMark from '@/components/WordMark'
+
+const Footer = () => {
+  return (
+    <div>
+      <WordMark />
+      Footer
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import WordMark from '@/components/WordMark'
+
+const Header = () => {
+  return (
+    <div>
+      <WordMark />
+    </div>
+  )
+}
+
+export default Header
